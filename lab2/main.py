15a73eb71aa861a2f278e901322b5be4e401e54f
Nazar <zazarenko@gmail.com>
2017-03-07 23:30:11 +0200

    Update main.py

--- lab2/main.py
+++ lab2/main.py
@@ -1,31 +1,54 @@
 import random
-class card(object):
-    suit_names = ["Clubs", "Diamonds", "Hearts", "Spades"]
-    rank_names = ["Ace", "2", "3", "4", "5", "6", "7",
-                  "8", "9", "10", "Jack", "Queen", "King"]
+
+class Card:
+
     def __init__(self, suit, rank):
         self.suit = suit
         self.rank = rank
 
-    def __str__(self):
-        return "%s of %s" % (self.rank(), self.suit())
 
-class deck(object):
+    def __repr__(self):
+        return"%s %s" % (self.suit, self.rank)
+
+class Deck:
     def __init__(self):
         self.cards = []
-        for suit in range(4):
-            for rank in range(1, 14):
-                card = Card(suit, rank)
+        for suit in ["Clubs", "Diamonds", "Hearts", "Spades"]:
+            for rank in range(1,14):
+                card = Card(rank, suit)
                 self.cards.append(card)
+        random.shuffle(self.cards)
 
     def __str__(self):
-        res = []
-        for card in self.cards:
-            res.append(str(card))
-        return '\n'.join(res)
+        return self.cards
 
-    def add_card(self, card):
-        self.cards.append(card)
+    def __next__(self):
+        return self.cards.pop()
 
-    def shuffle(self):
-        random.shuffle(self.cards)
+class Pyramid:
+    def __init__(self):
+        self.cards_pyramid = []
+
+    def __str__(self):
+        return self.cards_pyramid
+
+
+class Game:
+    def __init__(self):
+        self.deck = Deck()
+        self.pyramid = Pyramid()
+        for i in range(28):
+            self.pyramid.cards_pyramid.append(self.deck.__next__())
+
+    def __str__(self):
+        return self.pyramid.cards_pyramid
+
+
+if __name__ == "__main__":
+    mydeck = Deck()
+    print(mydeck.__str__())
+    mygame = Game()
+
+    print(mygame.__str__())
+    mypyramid = Pyramid()
+    print(mypyramid.__str__())
